f02bb2b43827a7ad05d2c459c041a1b01157da41
Bharani240296 <103750924+Bharani240296@users.noreply.github.com>
2023-02-07 13:43:47 +0530

    Add files via upload

---
+++ MessageDecode.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EndpointConfig;
+
+public class MessageDecode implements Decoder.Text<Messages> {
+private static Gson gson=new Gson();
+    @Override
+    public Messages decode(String s) throws DecodeException {
+        return gson.fromJson(s,Messages.class);
+    }
+
+    @Override
+    public boolean willDecode(String s) {
+        return s!=null;
+    }
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
